119c566e1448fcf663e10db2979d5a03f06a4e13
JJC <jjc@nlplab.cc>
2016-09-22 18:48:59 +0800

    fix wrong filename extension

--- js/members.js
+++ js/members.js
@@ -106,7 +106,7 @@ var members = {
             name: '林昌毅',
             alias: 'Chungyilin',
             research: '5 4 tusi be ba',
-            img: 'img/chungyilin.jpg',
+            img: 'img/chungyilin.gif',
             bkg_color: '#EEAD0E',
             intro: '故人西辭黃鶴樓，煙花三月下揚州。 孤帆遠影碧空盡，唯見長江天際流。',
             link: 'https://www.youtube.com/watch?v=HSHOU1OVbaY',
