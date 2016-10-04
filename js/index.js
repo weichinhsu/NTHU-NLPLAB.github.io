e28dcf3bfcfbef7f8d4e6550d14b0bbaa74455c3
shannywu <shanny@nlplab.cc>
2016-10-05 01:19:53 +0800

    members into semantic cards

--- js/index.js
+++ js/index.js
@@ -15,72 +15,226 @@ $(function() {
     });
 });
 
-$(function() {
-    var ms_block = '';
-    for( i in members['Master Students']) {
-        member = members['Master Students'][i];
-        name = escapeHtml(member.name);
-        alias = escapeHtml(member.alias);
-        img = escapeHtml(encodeURI(member.img));
-        research_area = escapeHtml(member.research);
-        bkg_color = escapeHtml(member.bkg_color);
-        intro = escapeHtml(member.intro);
-        link = (member.link) ? 'href="' + encodeURI(member.link) + '" target="_blank"': '';
-        
-        ms_block +=  
-        `<a class="col-md-3" ` + link + `>
-           <div class="thumbnail flipbox grad">
-             <div class="member-info front">
-               <img class="img-responsive" src="` + img + `" alt="` + name + `">
-               <h3>` + name + `<br ><small>` + alias + `</small></h3>
-               <div class="caption">
-                 <p>` + research_area + `</p>
-               </div>
-             </div>
-             <div class="member-info back" style="background-color:` + bkg_color + `;">
-               <h2>` + name + `</h2>
-               <p>` + intro + `</p>
-             </div>
-           </div>
-         </a>`;
-    }
-    $('div.ms-students > div:first-child').append(ms_block);
+function add_block(member_type) {
+  console.log(member_type);
+  var block = '';
+  for( i in members[member_type]) {
+      member = members[member_type][i];
+      name = escapeHtml(member.name);
+      alias = escapeHtml(member.alias);
+      img = escapeHtml(encodeURI(member.img));
+      research_area = escapeHtml(member.research);
+      color = escapeHtml(member.color);
+      intro = escapeHtml(member.intro);
+      link = (member.link) ? 'href="' + encodeURI(member.link) + '" target="_blank"': '';
+
+      block +=  `<div class="col-md-3">
+                      <div class="ui special cards">
+                        <div class="card ` + color + `">
+                          <div class="blurring dimmable image">
+                            <a class="ui dimmer" ` + link + `>
+                              <div class="content">
+                                <div class="center">
+                                  <div class="ui">` + intro + `</div>
+                                </div>
+                              </div>
+                            </a>
+                            <img src="` + img + `">
+                          </div>
+                          <div class="content">
+                            <div class="header">` + name + `</div>
+                            <div class="meta"><span>` + alias + `</span></div>
+                            <div class="description">` + research_area + `</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>`;
+  }
+  $('div.' + member_type).append(block);
+  $('.special.cards .image').dimmer({
+    on: 'hover'
+  });
+}
+
+$(function () {
+  console.log('test');
+  add_block('phd-students');
+  add_block('ms-students');
+  add_block('ra');
+  add_block('undergraduates');
 });
 
-$(function() {
-    var und_block = '';
-    for( i in members['Undergraduates']) {
-        member = members['Undergraduates'][i];
-        und_block +=  `<div class="col-md-3">
-        <div class="thumbnail flipbox und">
-            <div class="member-info front">
-                <h3>` + member.name + `<br><small>` + member.alias + `</small></h3>
-                        <div class="caption">
-                          <p>` + member.research + `</p>
-                        </div>
-                 </div></div>       
-                       </div>`;
-    }
-    $('div.undergrad > div:first-child').append(und_block);
-});
+// $(function() {
+//     var phd_block = '';
+//     for( i in members['PhD Students']) {
+//         member = members['PhD Students'][i];
+//         name = escapeHtml(member.name);
+//         alias = escapeHtml(member.alias);
+//         img = escapeHtml(encodeURI(member.img));
+//         research_area = escapeHtml(member.research);
+//         color = escapeHtml(member.color);
+//         intro = escapeHtml(member.intro);
+//         link = (member.link) ? 'href="' + encodeURI(member.link) + '" target="_blank"': '';
+//         // <div class="image">
+//         //   <img src="` + img + `">
+//         // </div>
+//         phd_block +=  `<div class="col-md-3">
+//                         <div class="ui special cards">
+//                           <div class="card ` + color + `">
+//                             <div class="blurring dimmable image">
+//                               <a class="ui dimmer" ` + link + `>
+//                                 <div class="content">
+//                                   <div class="center">
+//                                     <div class="ui">` + intro + `</div>
+//                                   </div>
+//                                 </div>
+//                               </a>
+//                               <img src="` + img + `">
+//                             </div>
+//                             <div class="content">
+//                               <div class="header">` + name + `</div>
+//                               <div class="meta"><span>` + alias + `</span></div>
+//                               <div class="description">` + research_area + `</div>
+//                             </div>
+//                           </div>
+//                         </div>
+//                       </div>`;
+//     }
+//     // <div class="extra content">
+//     //   <div><i class="users icon"></i>text</div>
+//     // </div>
+//     $('div.phd-students').append(phd_block);
+//     $('.special.cards .image').dimmer({
+//       on: 'hover'
+//     });
+//
+// });
+//
+//
+//
+// $(function() {
+//     var ms_block = '';
+//     for( i in members['Master Students']) {
+//         member = members['Master Students'][i];
+//         name = escapeHtml(member.name);
+//         alias = escapeHtml(member.alias);
+//         img = escapeHtml(encodeURI(member.img));
+//         research_area = escapeHtml(member.research);
+//         color = escapeHtml(member.color);
+//         intro = escapeHtml(member.intro);
+//         link = (member.link) ? 'href="' + encodeURI(member.link) + '" target="_blank"': '';
+//
+//         ms_block += `<div class="col-md-3">
+//                         <div class="ui special cards">
+//                           <div class="card ` + color + `">
+//                             <div class="blurring dimmable image">
+//                               <a class="ui dimmer" ` + link + `>
+//                                 <div class="content">
+//                                   <div class="center">
+//                                     <div class="ui">` + intro + `</div>
+//                                   </div>
+//                                 </div>
+//                               </a>
+//                               <img src="` + img + `">
+//                             </div>
+//                             <div class="content">
+//                               <div class="header">` + name + `</div>
+//                               <div class="meta"><span>` + alias + `</span></div>
+//                               <div class="description">` + research_area + `</div>
+//                             </div>
+//                           </div>
+//                         </div>
+//                       </div>`;
+//     }
+//     // console.log(ms_block);
+//     // <div class="extra content">
+//     //   <div><i class="users icon"></i>text</div>
+//     // </div>
+//     $('div.ms-students').append(ms_block);
+//     $('.special.cards .image').dimmer({
+//       on: 'hover'
+//     });
+// });
+//
+// $(function() {
+//     var ra_block = '';
+//     for( i in members['Research Assistant']) {
+//         member = members['Research Assistant'][i];
+//         ra_block += `<div class="col-md-3">
+//                         <div class="ui special cards">
+//                           <div class="card ` + member.color + `">
+//                             <div class="image">
+//                               <img src="` + member.img + `">
+//                             </div>
+//                             <div class="content">
+//                               <div class="header">` + member.name + `</div>
+//                               <div class="meta"><span>` + member.alias + `</span></div>
+//                             </div>
+//                           </div>
+//                         </div>
+//                       </div>`;
+//     }
+//     console.log(ra_block);
+//     // <div class="extra content">
+//     //   <div><i class="users icon"></i>text</div>
+//     // </div>
+//     $('div.ra').append(ra_block);
+//     $('.special.cards .image').dimmer({
+//       on: 'hover'
+//     });
+// });
+
+// $(function() {
+//     var und_block = '';
+//     for( i in members['Undergraduates']) {
+//         member = members['Undergraduates'][i];
+//         und_block +=  `<div class="col-md-3">
+//         <div class="thumbnail flipbox und">
+//             <div class="member-info front">
+//                 <h3>` + member.name + `<br><small>` + member.alias + `</small></h3>
+//                         <div class="caption">
+//                           <p>` + member.research + `</p>
+//                         </div>
+//                  </div></div>
+//                        </div>`;
+//     }
+//     $('div.undergrad > div:first-child').append(und_block);
+// });
 
 $(function() {
     var re_block = '';
     for( i in research['Research']) {
         re = research['Research'][i];
-        
+
         re_block +=  `<div class="col-md-4">
             <div class="research-item">
                 <span class="fa-stack fa-5x">
                   <i class="fa ` + re.fa + ` text-primary"></i>
                 </span>
-                <h4><strong>` + re.name_chi + `</strong></h4>
-                <h4><strong>` + re.name_eng + `</strong></h4>
+                <h3><strong>` + re.name_zh + `</strong></h3>
+                <h3><strong>` + re.name_en + `</strong></h3>
                 </div>
             </div>`;
-        console.log(re_block)
+        // console.log(re_block)
     }
     $('div.research').append(re_block);
 });
 
+$(function() {
+    var proj_block = '';
+    for( i in projects['Projects']) {
+        proj = projects['Projects'][i];
 
+        proj_block +=  `<div class="col-md-4">
+            <div class="project-item">
+                <a href="` + proj.link + `" target="_blank">
+                  <img class="img-project img-responsive" src="` + proj.img + `">
+                </a><br>
+                <p>` + proj.descrip_zh + `</p>
+                <p>` + proj.descrip_en + `</p>
+                </div>
+            </div>`;
+    }
+    // console.log(proj_block)
+    $('div.projects').append(proj_block);
+});
---
+++ js/projects.js
@@ -0,0 +1,22 @@
+var projects = {
+	'Projects': [
+        {
+            img: 'img/linggle.png',
+            link: 'http://linggle.com/',
+            descrip_zh: 'Linggle是一個語言搜尋引擎。它提供使用者快速且精準的英文慣用語以及搭配詞的檢索。',
+            descrip_en: 'Linggle is a Web-scale linguistics search engine, that retrieves lexical bundles in response to a given query.'
+        },
+        {
+            img: 'img/writeahead.png',
+            link: 'http://writeahead.nlpweb.org/',
+            descrip_zh: '互動密集的英語寫作環境，能在學習者寫作時提供即時提示，幫助他們寫得流暢又準確。',
+            descrip_en: 'WriteAhead is a resource-rich, Interactive Writing Environment that provides English learners with writing prompts, as well as "get it right" advice, to helps them write fluently and accurately.'
+        },
+        {
+            img: 'img/rephraser.png',
+            link: 'http://ironman.nlpweb.org:13142/',
+            descrip_zh: 'Rephraser2.0是一個幫助學習者增加句子豐富度的工具，替原本的句子重新包裝。',
+            descrip_en: 'Rephraser2.0 is a paraphrasing tool, which helps English learners enrich their’s articles. The system paraphrases sentences, expressing the same ideas with different words.'
+        },
+    ],
+}
