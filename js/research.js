98a5fadb66bdf03ca0d296bb43d68099b904b76d
shannywu <shanny@nlplab.cc>
2016-10-05 01:19:53 +0800

    members into semantic cards

--- js/research.js
+++ js/research.js
@@ -1,18 +1,18 @@
 var research = {
 	'Research': [
         {
-            name_chi: '文法改錯',
-            name_eng: 'Grammar Correction',
+            name_zh: '文法改錯',
+            name_en: 'Grammar Correction',
             fa: 'fa-check-square-o'
         },
         {
-            name_chi: '輔助寫作',
-            name_eng: 'Assisted Writing',
+            name_zh: '輔助寫作',
+            name_en: 'Assisted Writing',
             fa: 'fa-pencil-square-o'
         },
         {
-            name_chi: '機器翻譯',
-            name_eng: 'Machine Translation',
+            name_zh: '機器翻譯',
+            name_en: 'Mazhne Translation',
             fa: 'fa-globe'
         },
     ],
