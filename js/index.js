cf6be6a22730a23fa7324887ddafee061025537c
shannywu <shanny@nlplab.cc>
2016-10-27 22:52:30 +0800

    add projects

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ $(function() {
 
 function add_block(member_type) {
   console.log(member_type);
-  var block = '<div class="ui four doubling stackable special cards">';
+  var block = '';
   for( i in members[member_type]) {
       member = members[member_type][i];
       name = escapeHtml(member.name);
@@ -28,25 +28,28 @@ function add_block(member_type) {
       intro = escapeHtml(member.intro);
       link = (member.link) ? 'href="' + encodeURI(member.link) + '" target="_blank"': '';
 
-      block +=  `<div class="ui fluid card ` + color + `">
-                  <div class="blurring dimmable image">
-                    <a class="ui dimmer" ` + link + `>
-                      <div class="content">
-                        <div class="center">
-                          <div class="ui">` + intro + `</div>
-                        </div>
+      block +=  `<div class="col-md-3">
+                  <div class="ui special cards">
+                    <div class="card ` + color + `">
+                      <div class="blurring dimmable image">
+                        <a class="ui dimmer" ` + link + `>
+                          <div class="content">
+                            <div class="center">
+                              <div class="ui">` + intro + `</div>
+                            </div>
+                          </div>
+                        </a>
+                        <img src="` + img + `">
                       </div>
-                    </a>
-                    <img src="` + img + `">
-                  </div>
-                  <div class="content">
-                    <div class="header">` + name + `</div>
-                    <div class="meta"><span>` + alias + `</span></div>
-                    <div class="description">` + research_area + `</div>
+                      <div class="content">
+                        <div class="header">` + name + `</div>
+                        <div class="meta"><span>` + alias + `</span></div>
+                        <div class="description">` + research_area + `</div>
+                      </div>
+                    </div>
                   </div>
                 </div>`;
   }
-  block +=  `</div>`;
   $('div.' + member_type).append(block);
   $('.special.cards .image').dimmer({
     on: 'hover'
@@ -61,161 +64,45 @@ $(function () {
   add_block('undergraduates');
 });
 
-// $(function() {
-//     var phd_block = '';
-//     for( i in members['PhD Students']) {
-//         member = members['PhD Students'][i];
-//         name = escapeHtml(member.name);
-//         alias = escapeHtml(member.alias);
-//         img = escapeHtml(encodeURI(member.img));
-//         research_area = escapeHtml(member.research);
-//         color = escapeHtml(member.color);
-//         intro = escapeHtml(member.intro);
-//         link = (member.link) ? 'href="' + encodeURI(member.link) + '" target="_blank"': '';
-//         // <div class="image">
-//         //   <img src="` + img + `">
-//         // </div>
-//         phd_block +=  `<div class="col-md-3">
-//                         <div class="ui special cards">
-//                           <div class="card ` + color + `">
-//                             <div class="blurring dimmable image">
-//                               <a class="ui dimmer" ` + link + `>
-//                                 <div class="content">
-//                                   <div class="center">
-//                                     <div class="ui">` + intro + `</div>
-//                                   </div>
-//                                 </div>
-//                               </a>
-//                               <img src="` + img + `">
-//                             </div>
-//                             <div class="content">
-//                               <div class="header">` + name + `</div>
-//                               <div class="meta"><span>` + alias + `</span></div>
-//                               <div class="description">` + research_area + `</div>
-//                             </div>
-//                           </div>
-//                         </div>
-//                       </div>`;
-//     }
-//     // <div class="extra content">
-//     //   <div><i class="users icon"></i>text</div>
-//     // </div>
-//     $('div.phd-students').append(phd_block);
-//     $('.special.cards .image').dimmer({
-//       on: 'hover'
-//     });
-//
-// });
-//
-//
-//
-// $(function() {
-//     var ms_block = '';
-//     for( i in members['Master Students']) {
-//         member = members['Master Students'][i];
-//         name = escapeHtml(member.name);
-//         alias = escapeHtml(member.alias);
-//         img = escapeHtml(encodeURI(member.img));
-//         research_area = escapeHtml(member.research);
-//         color = escapeHtml(member.color);
-//         intro = escapeHtml(member.intro);
-//         link = (member.link) ? 'href="' + encodeURI(member.link) + '" target="_blank"': '';
-//
-//         ms_block += `<div class="col-md-3">
-//                         <div class="ui special cards">
-//                           <div class="card ` + color + `">
-//                             <div class="blurring dimmable image">
-//                               <a class="ui dimmer" ` + link + `>
-//                                 <div class="content">
-//                                   <div class="center">
-//                                     <div class="ui">` + intro + `</div>
-//                                   </div>
-//                                 </div>
-//                               </a>
-//                               <img src="` + img + `">
-//                             </div>
-//                             <div class="content">
-//                               <div class="header">` + name + `</div>
-//                               <div class="meta"><span>` + alias + `</span></div>
-//                               <div class="description">` + research_area + `</div>
-//                             </div>
-//                           </div>
-//                         </div>
-//                       </div>`;
-//     }
-//     // console.log(ms_block);
-//     // <div class="extra content">
-//     //   <div><i class="users icon"></i>text</div>
-//     // </div>
-//     $('div.ms-students').append(ms_block);
-//     $('.special.cards .image').dimmer({
-//       on: 'hover'
-//     });
-// });
-//
-// $(function() {
-//     var ra_block = '';
-//     for( i in members['Research Assistant']) {
-//         member = members['Research Assistant'][i];
-//         ra_block += `<div class="col-md-3">
-//                         <div class="ui special cards">
-//                           <div class="card ` + member.color + `">
-//                             <div class="image">
-//                               <img src="` + member.img + `">
-//                             </div>
-//                             <div class="content">
-//                               <div class="header">` + member.name + `</div>
-//                               <div class="meta"><span>` + member.alias + `</span></div>
-//                             </div>
-//                           </div>
-//                         </div>
-//                       </div>`;
-//     }
-//     console.log(ra_block);
-//     // <div class="extra content">
-//     //   <div><i class="users icon"></i>text</div>
-//     // </div>
-//     $('div.ra').append(ra_block);
-//     $('.special.cards .image').dimmer({
-//       on: 'hover'
-//     });
-// });
-
-// $(function() {
-//     var und_block = '';
-//     for( i in members['Undergraduates']) {
-//         member = members['Undergraduates'][i];
-//         und_block +=  `<div class="col-md-3">
-//         <div class="thumbnail flipbox und">
-//             <div class="member-info front">
-//                 <h3>` + member.name + `<br><small>` + member.alias + `</small></h3>
-//                         <div class="caption">
-//                           <p>` + member.research + `</p>
-//                         </div>
-//                  </div></div>
-//                        </div>`;
-//     }
-//     $('div.undergrad > div:first-child').append(und_block);
-// });
-
 $(function() {
     var re_block = '';
     for( i in research['Research']) {
         re = research['Research'][i];
 
         re_block +=  `<div class="col-md-4">
-            <div class="research-item">
-                <span class="fa-stack fa-5x">
-                  <i class="fa ` + re.fa + ` text-primary"></i>
-                </span>
-                <h3><strong>` + re.name_zh + `</strong></h3>
-                <h3><strong>` + re.name_en + `</strong></h3>
-                </div>
-            </div>`;
+                        <div class="research-item">
+                          <span class="fa-stack fa-5x">
+                            <i class="fa ` + re.fa + ` text-primary"></i>
+                          </span>
+                          <h3><strong>` + re.name_zh + `</strong></h3>
+                          <h3><strong>` + re.name_en + `</strong></h3>
+                          </div>
+                      </div>`;
         // console.log(re_block)
     }
     $('div.research').append(re_block);
 });
+
+// $(function() {
+//     var proj_block = '';
+//     for( i in projects['Projects']) {
+//         proj = projects['Projects'][i];
+
+//         proj_block +=  `<div class="col-md-4">
+//                           <div class="project-item">
+//                             <a href="` + proj.link + `" target="_blank">
+//                               <img class="img-project img-responsive" src="` + proj.img + `">
+//                             </a><br>
+//                             <a href="` + proj.label_link + `" target="_blank" class="label label-gray">` + proj.label + `</a>
+//                             <p>` + proj.descrip_zh + `</p>
+//                             <p>` + proj.descrip_en + `</p>
+//                           </div>
+//                         </div>`;
+//     }
+//     // console.log(proj_block)
+//     $('div.projects').append(proj_block);
+// });
+
 
 $(function() {
     var proj_block = '';
@@ -223,14 +110,22 @@ $(function() {
         proj = projects['Projects'][i];
 
         proj_block +=  `<div class="col-md-4">
-            <div class="project-item">
-                <a href="` + proj.link + `" target="_blank">
-                  <img class="img-project img-responsive" src="` + proj.img + `">
-                </a><br>
-                <p>` + proj.descrip_zh + `</p>
-                <p>` + proj.descrip_en + `</p>
-                </div>
-            </div>`;
+                          <div class="panel panel-default panel-proj">
+                            <div class="panel-heading">
+                                <h4>` + proj.title + `</h4>
+                            </div>
+                            <div class="panel-body">
+                              <a href="` + proj.link + `" target="_blank">
+                                <img class="media-object img-proj" src="` + proj.img + `">
+                              </a>
+                              <p>` + proj.descrip_zh + `</p>
+                              <p>` + proj.descrip_en + `</p>
+                              <a href="` + proj.label_link + `" target="_blank">
+                                <div class="label label-gray">` + proj.label + `</div>
+                              </a>
+                            </div>
+                          </div>
+                        </div>`;
     }
     // console.log(proj_block)
     $('div.projects').append(proj_block);
