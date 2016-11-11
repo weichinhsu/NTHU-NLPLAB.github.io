fb8b8284fe1f8418319c513234874ff76596032a
JJC <jjc@nlplab.cc>
2016-11-11 21:29:07 +0800

    es5, compatible with IE11

--- js/index.js
+++ js/index.js
@@ -37,16 +37,15 @@ $(function() {
     for( i in research['Research']) {
         re = research['Research'][i];
 
-        re_block +=  `<div class="col-md-4">
-                        <div class="research-item">
-                          <span class="fa-stack fa-5x">
-                            <i class="fa ${re.fa}text-primary"></i>
-                          </span>
-                          <h3><strong>${re.name_zh}</strong></h3>
-                          <h3><strong>${re.name_en}</strong></h3>
-                          </div>
-                      </div>`;
-        // console.log(re_block)
+        re_block +=  '<div class="col-md-4"> \
+                        <div class="research-item"> \
+                          <span class="fa-stack fa-5x"> \
+                            <i class="fa ' + re.fa + ' text-primary"></i> \
+                          </span> \
+                          <h3><strong>' + re.name_zh + '</strong></h3> \
+                          <h3><strong>' + re.name_en + '</strong></h3> \
+                          </div> \
+                      </div>';
     }
     $('div.research').append(re_block);
 });
@@ -56,32 +55,31 @@ $(function() {
     for( i in projects) {
         proj = projects[i];
 
-        proj_block +=  '<div class="ui raised link card" href="http://www.dog.com">';
-        proj_block +=  `<div class="project content title">
-                          <div class="header">${proj.title}</div>
-                        </div>`;
-        proj_block +=  `<div class="blurring dimmable project image content">
-                          <a class="ui  dimmer" href="${proj.link}" target="_blank">
-                            <div class="content">
-                              <div class="center">
-                                <div class="ui primary button">Go to</div>
-                              </div>
-                            </div>
-                          </a>
-                          <img src="${proj.img}">
-                        </div>`;
-        proj_block +=  `<div class="project content description text-justify">
-                          <div class="description">
-                            <p>${proj.descrip_zh}</p>
-                            <p>${proj.descrip_en}</p>
-                          </div>
-                        </div>`;
+        proj_block +=  '<div class="ui raised link card">';
+        proj_block +=  '<div class="project content title"> \
+                          <div class="header">' + proj.title + '</div></div>';
+        proj_block +=  '<div class="blurring dimmable project image content"> \
+                          <a class="ui  dimmer" href="' + proj.link + '" target="_blank"> \
+                            <div class="content"> \
+                              <div class="center"> \
+                                <div class="ui primary button">Go to</div> \
+                              </div> \
+                            </div> \
+                          </a> \
+                          <img src="' + proj.img + '"> \
+                        </div>';
+        proj_block +=  '<div class="project content description text-justify"> \
+                          <div class="description"> \
+                            <p>' + proj.descrip_zh + '</p> \
+                            <p>' + proj.descrip_en + '</p> \
+                          </div> \
+                        </div>';
         if(proj.label) {
-            proj_block +=  `<div class="extra project content center aligned">
-                              <a href="${proj.label_link}" target="_blank">
-                                <div class="label project">${proj.label}</div>
-                              </a>
-                            </div>`;
+            proj_block +=  '<div class="extra project content center aligned"> \
+                              <a href="' + proj.label_link + '" target="_blank"> \
+                                <div class="label project">' + proj.label + '</div> \
+                              </a> \
+                            </div>';
         }
         proj_block +=  '</div>';
     }
@@ -107,25 +105,25 @@ function add_block(member_type) {
       intro = escapeHtml(member.intro);
       link = (member.link) ? 'href="' + encodeURI(member.link) + '" target="_blank"': '';
 
-      block +=  `<div class="ui fluid card ${color}">
-                  <div class="blurring dimmable image">
-                    <a class="ui dimmer" ${link}>
-                      <div class="content">
-                        <div class="center">
-                          <div class="ui">${intro}</div>
-                        </div>
-                      </div>
-                    </a>
-                    <img src="${img}">
-                  </div>
-                  <div class="content">
-                    <div class="header">${name}</div>
-                    <div class="meta"><span>${alias}</span></div>
-                    <div class="description">${research_area}</div>
-                  </div>
-                </div>`;
+      block +=  '<div class="ui fluid card ' + color + '"> \
+                  <div class="blurring dimmable image"> \
+                    <a class="ui dimmer" ' + link + '> \
+                      <div class="content"> \
+                        <div class="center"> \
+                          <div class="ui">' + intro + '</div> \
+                        </div> \
+                      </div> \
+                    </a> \
+                    <img src="' + img + '"> \
+                  </div> \
+                  <div class="content"> \
+                    <div class="header">' + name + '</div> \
+                    <div class="meta"><span>' + alias + '</span></div> \
+                    <div class="description">' + research_area + '</div> \
+                  </div> \
+                </div>';
   }
-  block +=  `</div>`;
+  block +=  '</div>';
   $('div.' + member_type).append(block);
   $('.special.cards .image').dimmer({
     on: 'hover'
